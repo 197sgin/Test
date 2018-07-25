72095530e4b42abe989899d1216e1123d9184416
197sgin <v_chenjun@weshareholdings.com>
2018-07-25 19:29:17 +0800

    123

---
+++ 2.java
@@ -0,0 +1,18 @@
+package com.weshare.asset.factory;
+
+/**
+ * @ClassName 测试
+ * @Description TODO
+ * @Author
+ * @Date 14:18
+ * @Version 1.0
+ **/
+public class cheshi {
+
+    private String cheshi;//测试上传
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
