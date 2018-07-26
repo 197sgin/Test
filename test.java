951281cf4d1c1cfe7089040a7f405255ed3bf11b
197sgin <v_chenjun@weshareholdings.com>
2018-07-26 17:56:17 +0800

    test 0

---
+++ test.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName 测试
+ * @Description TODO
+ * @Author
+ * @Date 14:18
+ * @Version 1.0
+ **/
+public class test {
+
+    private String cheshi;//测试上传
+    private String CCC;//测试上传
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    
+}
